54c1d6f75631f62848cb3c8058a3447dd9ed93eb
Pieter Germishuys| <pieter@pieterg.com>
2018-10-04 19:39:18 +0200

    Let's get mean

--- build.ps1
+++ build.ps1
@@ -1,27 +1,2 @@
-[CmdletBinding()]
-Param(
-    [string]$Target = "default"
-)
-
 $ErrorActionPreference = "Stop";
-
-# Install .NET Core
-
-$dotNetVersionString = dotnet --version
-$dotNetVersion = [Version]$dotnetVersionString
-$minDotNetVersionString = "2.1.400"
-$minDotNetVersion = [Version]$minDotNetVersionString 
-
-If($dotnetVersion -lt $minDotnetVersion){
-    "Installing .Net SDK $minDotNetVersion..."
-    Get-Process "dotnet" | Stop-Process
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    & ./dotnet-install.ps1 -Channel Current -Version $minDotNetVersionString
-}
-else{
-    ".Net SDK $dotNetVersionString installed"
-}
-
-# Run Build
-
-dotnet run -p build\build.csproj --project $Target
+dotnet run --project build -- $args
