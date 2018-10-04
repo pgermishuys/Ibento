737b3954dc8252a5af7ade904e13a5cea9e36850
Pieter Germishuys <pieter@pieterg.com>
2018-10-04 19:47:29 +0200

    Lean shell script

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DOTNET_INSTALL_PATH=$SCRIPT_DIR/.dotnet
-echo "${0##*/}": Installing DotNetCore.
-curl -sSL https://dot.net/dotnet-install.sh | bash /dev/stdin --channel current --version 2.1.401 --install-dir $DOTNET_INSTALL_PATH
 
-echo "${0##*/}": Running Build.
-dotnet run -p build/build.csproj --project "$@"
+dotnet run --project build -- "$@"
